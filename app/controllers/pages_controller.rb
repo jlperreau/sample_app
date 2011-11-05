0a789c2a4210315cc31f73982a66af30079a0e81
jlperreau <jlperreau@nerim.net>
2011-11-05 20:09:42 +0100

    removed default Rails page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,17 @@
 class PagesController < ApplicationController
   def home
+	  @titre = "Accueil"
   end
 
   def contact
+	  @titre = "Contact"
   end
 
   def about
+	  @titre = "A Propos"
+  end
+
+  def help
+	@titre = "Aide"
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+	#retourne un titre basé sur la page
+def titre
+	base_titre = "Simple App du Tutoriel Ruby on rails"
+	if @titre.nil?
+		base_titre
+	else
+		"#{base_titre} | #{@titre}"
+	end
+end
+
 end
